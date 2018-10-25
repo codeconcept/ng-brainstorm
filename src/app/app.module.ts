2c4f4bafdb8f09e8257f2ac826320eb5af080ff5
codeconcept <contact@codeconcept.fr>
2018-10-25 17:52:36 +0200

    simple drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 import { HomeComponent } from './home/home.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
 
@@ -16,7 +18,8 @@ import { WhiteBoardComponent } from './white-board/white-board.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/white-board/white-board.component.ts
+++ src/app/white-board/white-board.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhiteBoardComponent implements OnInit {
 
+  ideas = [
+    {
+      id: 1, 
+      title: 'Veille techno Angular',
+      details: 'Faire un POC avec les nouveautés de la V7'
+    },
+    {
+      id: 2, 
+      title: 'Vue et Vuex',
+      details: 'Trouver une idée d\'appli pour illuser Vue et Vuex'
+    },
+    {
+      id: 3, 
+      title: 'Impôts locaux',
+      details: 'Quel montant réel'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
